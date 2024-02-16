efd653b662424677b7031a5544171414fa9667f6
givenjeremia <u794158688@id-dci-web1157.main-hosting.eu>
2024-02-16 15:23:43 +0000

    ga ngaruh

--- app/Http/Controllers/DailyTaskController.php
+++ app/Http/Controllers/DailyTaskController.php
@@ -30,7 +30,6 @@ class DailyTaskController extends Controller
     public function tableData(){
         $user = Auth::user();
         $user = User::find($user->id);
-
         if($user->hasRole('staff')){
             $dailyTask = DailyTask::where('user_id',$user->id)->get();
         }
